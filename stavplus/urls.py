05c07cff91a3ec2bd1d96fd6fa1d6a095e001653
Bakanov Artem <bakanovartem@Bakanovs-MacBook.local>
2026-08-08 20:49:22 +0300

    feat: finalize editorial header and social navigation

--- stavplus/urls.py
+++ stavplus/urls.py
@@ -2,6 +2,8 @@
 StavPlus URL Configuration
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from news import views
 
@@ -20,3 +22,6 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
